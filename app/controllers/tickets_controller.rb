813ada5938382a8e6fa457d8010313d627e2e6f8
Gearz <shanegeary@yahoo.com>
2020-10-30 17:48:01 -0600

    Added Venues controller actions

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,26 @@
 class TicketsController < ApplicationController
     before_action :not_logged_in
 
+    def index 
+        @tickets = Ticket.all 
+    end
+
     def new 
         @ticket = Ticket.new 
+    end
+
+    def create 
+        @ticket = current_user.tickets.build(ticket_params) 
+        if @ticket.save  
+            redirect_to tickets_path  
+        else 
+            render :new 
+        end
+    end
+
+    private 
+
+    def ticket_params 
+        params.require(:ticket).permit(:name, :date, :artist) 
     end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,36 @@
 class VenuesController < ApplicationController
+
+    def index 
+        @venues = Venue.all 
+    end
+
+    def new 
+        @venue = Venue.new 
+    end
+
+    def create 
+        @venue = current_user.venues.build(venue_params) 
+        if @venue.save  
+            redirect_to venues_path  
+        else 
+            render :new 
+        end
+    end
+
+    def show 
+        @venue = Venue.find_by(id: params[:id])
+    end
+
+    def edit 
+        @venue = Venue.find_by(id: params[:id])
+    end
+
+    def update 
+        @venue = Venue.find_by(id: params[:id])
+        if @venue.update(venue_params)
+            redirect_to venue_path(@venue)
+        else
+            render :edit
+        end
+    end
 end
