2e1ea2397ca798c8b6723ebeca0746e42d31fb61
Gearz <shanegeary@yahoo.com>
2020-11-03 17:16:18 -0700

    link to see all venues in nav bar, attempting to add venues name along side list of corresponding tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -41,6 +41,6 @@ class TicketsController < ApplicationController
     private 
 
     def ticket_params 
-        params.require(:ticket).permit(:name, :date, :artist) 
+        params.require(:ticket).permit(:name, :date, :artist, :venue_id) 
     end
 end
